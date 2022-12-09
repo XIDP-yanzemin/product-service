927842f8ee416dda2358f25f5cdcd5b795c0ca68
josieyan123 <josieyan123@gmail.com>
2022-12-09 23:16:39 +0800

    fix: return total number of element when list

--- src/main/java/com/practice/productservice/response/CommonPageModel.java
+++ src/main/java/com/practice/productservice/response/CommonPageModel.java
@@ -1,9 +1,11 @@
 package com.practice.productservice.response;
 
+import com.practice.productservice.entity.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -20,14 +22,17 @@ public class CommonPageModel<T> {
 
     protected Integer pageSize;
 
-    protected Integer numberOfElements;
+    protected Long numberOfElements;
 
-    public static CommonPageModel<ProductResponseForPage> from(Pageable pageable, List<ProductResponseForPage> responses) {
+    public static CommonPageModel<ProductResponseForPage> from(
+            Page<Product> products,
+            Pageable pageable,
+            List<ProductResponseForPage> responses) {
         return CommonPageModel.<ProductResponseForPage>builder()
                 .content(responses)
                 .pageNumber(pageable.getPageNumber())
                 .pageSize(pageable.getPageSize())
-                .numberOfElements(responses.size())
+                .numberOfElements(products.getTotalElements())
                 .build();
     }
 }
