cee21ce6e57dbe8a907f125aab7c8fe452b22745
josieyan123 <josieyan123@gmail.com>
2022-12-10 23:12:14 +0800

    feat: add getUsersByIdList method

--- src/main/java/com/practice/productservice/client/UserFeignService.java
+++ src/main/java/com/practice/productservice/client/UserFeignService.java
@@ -2,8 +2,11 @@ package com.practice.productservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @FeignClient(name = "user-service", url = "http://localhost:8081/users")
 public interface UserFeignService {
@@ -12,4 +15,7 @@ public interface UserFeignService {
     //todo 不需要加 /client 和普通的 API 没有区别
     @RequestMapping(method = RequestMethod.GET, path = "/client/{id}")
     ListUserResponse getUserById(@PathVariable Long id);
+
+    @RequestMapping(method = RequestMethod.GET, path = "/client/query")
+    List<ListUserResponse> getUsersByIdList(@RequestBody List<Long> idList);
 }
