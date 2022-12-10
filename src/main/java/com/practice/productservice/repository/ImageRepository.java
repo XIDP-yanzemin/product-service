2e985822200686b2f811e4129346349e0945262d
josieyan123 <josieyan123@gmail.com>
2022-12-10 23:02:49 +0800

    refactor: remove unused method

--- src/main/java/com/practice/productservice/repository/ImageRepository.java
+++ src/main/java/com/practice/productservice/repository/ImageRepository.java
@@ -2,17 +2,8 @@ package com.practice.productservice.repository;
 
 import com.practice.productservice.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
-    @Modifying
-    @Query("DELETE FROM products_image_info p WHERE p.product.id= ?1")
-    void deleteByProductId(Long productId);
-
-    List<Image> findByProductIdIn(List<Long> idList);
 }
