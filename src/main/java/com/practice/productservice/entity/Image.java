f95b4c1ab8bae0948daebb3f8bf8e12e1d5bf7bf
josieyan123 <josieyan123@gmail.com>
2022-12-10 23:02:00 +0800

    refactor: rename table name & remove unused method

--- src/main/java/com/practice/productservice/entity/Image.java
+++ src/main/java/com/practice/productservice/entity/Image.java
@@ -5,16 +5,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
-@Entity(name = "products_image_info")
+@Entity(name = "images")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,13 +21,5 @@ public class Image {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinColumn(name = "product_id")
-    private Product product;
-
     private String url;
-
-    public static Image relateUrlToProduct(Product product, String url) {
-        return Image.builder().url(url).product(product).build();
-    }
 }
